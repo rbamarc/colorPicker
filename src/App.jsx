4c42f1c00762a2e971a8aaba336f0cf842ca7d6e
rbamarc <rbamarc929@gmail.com>
2023-07-31 06:17:08 -0700

    made a color picker

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
+import { useState } from "react";
+
 // Write your Color component here
+const Color = ({ color, selectedColor, setSelectedColor }) => {
+  const isSelected = selectedColor === color;
+
+  return (
+    <div
+      className={`color-circle ${color} ${isSelected ? "selected" : ""}`}
+      onClick={() => setSelectedColor(color)}
+    ></div>
+  );
+};
 
 const App = () => {
+  const [selectedColor, setSelectedColor] = useState("");
+
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id="colors-list">
+        <Color color="red" selectedColor={selectedColor} setSelectedColor={setSelectedColor} />
+        <Color color="blue" selectedColor={selectedColor} setSelectedColor={setSelectedColor} />
+        <Color color="green" selectedColor={selectedColor} setSelectedColor={setSelectedColor} />
+      </div>
     </div>
   );
 };
